dc767589d6574ad4d8aa3d0298588e9854a9a3a5
Brian Williams <152632135+bkwilliamsmerncode@users.noreply.github.com>
2024-03-03 21:21:23 -0700

    Updates

---
+++ precache-manifest.fc2d846950cca0671b49b445d96f7ce8.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "228923ae76c60926a38b80864541bf04",
+    "url": "/Brian-Williams-Portfolio/index.html"
+  },
+  {
+    "revision": "6ee93e49576034e2ad8e",
+    "url": "/Brian-Williams-Portfolio/static/css/main.2814e9bf.chunk.css"
+  },
+  {
+    "revision": "db702b2536d5543ef463",
+    "url": "/Brian-Williams-Portfolio/static/js/2.b5a9bd87.chunk.js"
+  },
+  {
+    "revision": "6ee93e49576034e2ad8e",
+    "url": "/Brian-Williams-Portfolio/static/js/main.30964d6c.chunk.js"
+  },
+  {
+    "revision": "105bbf6960c9e607bf12",
+    "url": "/Brian-Williams-Portfolio/static/js/runtime~main.9fe5e841.js"
+  },
+  {
+    "revision": "ba5e10ff92b765620a1c9530a462d5ca",
+    "url": "/Brian-Williams-Portfolio/static/media/RealEstate.ba5e10ff.png"
+  },
+  {
+    "revision": "d281cf71c53049c10678c78c51c9a12b",
+    "url": "/Brian-Williams-Portfolio/static/media/Tenzies.d281cf71.png"
+  },
+  {
+    "revision": "99b0ff41994c89c9a198e20305143010",
+    "url": "/Brian-Williams-Portfolio/static/media/calculator.99b0ff41.png"
+  },
+  {
+    "revision": "abded566fae6e186cb05e57ac754cdac",
+    "url": "/Brian-Williams-Portfolio/static/media/crypto.abded566.png"
+  },
+  {
+    "revision": "bf9976a32600bb0ac99bada9cbbd1048",
+    "url": "/Brian-Williams-Portfolio/static/media/css.bf9976a3.png"
+  },
+  {
+    "revision": "9c3a7a74b6669759d2e48022ca901649",
+    "url": "/Brian-Williams-Portfolio/static/media/es6.9c3a7a74.jpg"
+  },
+  {
+    "revision": "460ccf3b35020bd28ca103be00620cda",
+    "url": "/Brian-Williams-Portfolio/static/media/github.460ccf3b.png"
+  },
+  {
+    "revision": "38040658eeae2cda47fa1d59b6d2f8ee",
+    "url": "/Brian-Williams-Portfolio/static/media/gym.38040658.png"
+  },
+  {
+    "revision": "0b3218a70ddda2621710099ab76077fc",
+    "url": "/Brian-Williams-Portfolio/static/media/hacker2.0b3218a7.png"
+  },
+  {
+    "revision": "a470966fd77afe1802b84526259b0523",
+    "url": "/Brian-Williams-Portfolio/static/media/html.a470966f.png"
+  },
+  {
+    "revision": "2a6f0bb78a736331d3ff140f8140436d",
+    "url": "/Brian-Williams-Portfolio/static/media/jquery.2a6f0bb7.jpg"
+  },
+  {
+    "revision": "7fddb995ddc768e1cbca1ed270ccb1a9",
+    "url": "/Brian-Williams-Portfolio/static/media/materialui.7fddb995.png"
+  },
+  {
+    "revision": "6210dd18e2d873c6e1b84d5d098662f4",
+    "url": "/Brian-Williams-Portfolio/static/media/mongo.6210dd18.png"
+  },
+  {
+    "revision": "896b0753fe8bc3b4acdf34edf7981066",
+    "url": "/Brian-Williams-Portfolio/static/media/mongodb2.896b0753.png"
+  },
+  {
+    "revision": "839a4dd2e0d115ba82a08d0727fdf2d3",
+    "url": "/Brian-Williams-Portfolio/static/media/node.839a4dd2.png"
+  },
+  {
+    "revision": "2bb50916246a6cc98d6b5809e73ab7f9",
+    "url": "/Brian-Williams-Portfolio/static/media/passcode.2bb50916.png"
+  },
+  {
+    "revision": "614ce4b66dc6e2500caab6cd69cad807",
+    "url": "/Brian-Williams-Portfolio/static/media/react.614ce4b6.png"
+  },
+  {
+    "revision": "39a29c40f1abec783e9f8cd4472d166a",
+    "url": "/Brian-Williams-Portfolio/static/media/reactjs2.39a29c40.png"
+  }
+]);
